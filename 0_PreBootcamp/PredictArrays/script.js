3640939e367599fe87df5067309d943152cace00
Joshua Valdez <josh@augnos.com>
2022-09-23 22:16:56 -0700

    pre-bootcamp stuff

---
+++ 0_PreBootcamp/PredictArrays/script.js
@@ -0,0 +1,6 @@
+function displayContactInfo() {
+    var auntContactInfo = ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+
+displayContactInfo();
