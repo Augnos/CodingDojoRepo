cec88dc7dcd95c8a0a139f5cbd6b07018c0d61e8
Joshua Valdez <josh@augnos.com>
2022-09-23 21:06:37 -0700

    Python setup

---
+++ 0_PreBootcamp/Predict/script.js
@@ -0,0 +1,24 @@
+function myBirthYearFunc() {
+    console.log("I was born in " + 1980);
+}
+// prediction 1: "I was born in 1980"
+
+function myBirthYearFunc(birthYearInput) {
+    console.log("I was born in " + birthYearInput);
+}
+
+// prediction 2: I was born in 1980
+
+function add(num1, num2) {
+    console.log("Summing Numbers!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+
+// prediction 3: 
+// Summing Numbers!
+// num1 is: 10
+// num2 is: 20
+// 30
