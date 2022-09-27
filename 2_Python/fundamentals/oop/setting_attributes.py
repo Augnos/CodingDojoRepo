28bc6cf032aecbe4a80bdfeb64d378ed8acf481d
Joshua Valdez <josh@augnos.com>
2022-09-27 16:30:12 -0700

    OOP training

---
+++ 2_Python/fundamentals/oop/setting_attributes.py
@@ -0,0 +1,24 @@
+class Shoe:
+    # now our method has 4 parameters (including self)!
+    def __init__(self, brand, shoe_type, price):
+        # we assign them accordingly
+        self.brand = brand
+        self.type = shoe_type
+        self.price = price
+        # the status is set to True by default
+        self.in_stock = True
+
+skater_shoe = Shoe("Vans", "Low-top Trainers", 59.99)
+dress_shoe = Shoe("Jack & Jill Bootery", "Ballet Flats", 29.99)
+print(skater_shoe.type)	# output: Low-top Trainers
+print(dress_shoe.type)	# output: Ballet Flats
+
+# Ninja Challenges!
+# Open this code on the Trace website to get a better view of all the variables and their attributes.
+# Make a new instance of a shoe
+combat_boot = Shoe("Dr. Martens", "Steel-toe Boot", 249.99)
+print(combat_boot.brand)
+
+# Update the in_stock attribute to False
+combat_boot.in_stock = False
+print(combat_boot.in_stock)
