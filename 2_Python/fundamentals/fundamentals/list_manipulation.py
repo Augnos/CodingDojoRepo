a81a1ce9995ea8a1bdb7c7d965da059f03e50b3e
Joshua Valdez <josh@augnos.com>
2022-09-26 17:07:19 -0700

    For Loop Basic I completed

---
+++ 2_Python/fundamentals/fundamentals/list_manipulation.py
@@ -0,0 +1,13 @@
+countries = ["Uganda", "Chile", "Albania", "Saudi Arabia"]
+
+# Challenge 1: Fix the range!
+for integer in range(4):
+    print(str(integer) + ":")
+    # Challenge 2: print the index here
+    print(countries[integer])
+    # Challenge 3: print the country here
+
+# Looping over values only...
+for x in countries:
+    print(x)
+    # Challenge 4: print the country here
