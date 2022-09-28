2ff173ddfbb98d64b92774863d7c93fbb9797f97
Joshua Valdez <josh@augnos.com>
2022-09-28 15:47:37 -0700

    Playground completed

--- 2_Python/flask/fundamentals/playground/server.py
+++ 2_Python/flask/fundamentals/playground/server.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/play')
-def hello_world():
-    return render_template('index.html')
 
+@app.route('/play')
+def level1():
+    return render_template('index.html', times = 3, bgColor = "#9fc5f8")
+
+@app.route('/play/<int:count>')
+def level2(count):
+    return render_template('index.html', times = count, bgColor = "#9fc5f8")
+
+@app.route('/play/<int:count>/<color>')
+def level3(count, color):
+    return render_template('index.html', times = count, bgColor = color)
 
 if __name__=="__main__":
     app.run(debug=True)
