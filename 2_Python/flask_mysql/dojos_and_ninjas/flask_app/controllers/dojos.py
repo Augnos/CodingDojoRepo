c3b3832b9bbddb2d23dadc5f24fd92ef9721650f
Joshua Valdez <josh@augnos.com>
2022-10-06 17:19:45 -0700

    Working on dojos & ninjas

--- 2_Python/flask_mysql/dojos_and_ninjas/flask_app/controllers/dojos.py
+++ 2_Python/flask_mysql/dojos_and_ninjas/flask_app/controllers/dojos.py
@@ -13,67 +13,67 @@ def index():
 def dojos():
     dojos = Dojo.get_all()
     print(dojos)
-    return render_template("read_all.html", all_dojos = dojos)
+    return render_template("dojos.html", all_dojos = dojos)
 
 
-# ---------- New Dojo Page ----------- done
-@app.route("/dojos/new")
-def new():
-    return render_template("create.html")
+# # ---------- New Dojo Page ----------- done
+# @app.route("/dojos/new")
+# def new():
+#     return render_template("create.html")
 
 
-# ---------- Create New Dojo Post ----------- done
-@app.route('/create_dojo', methods=["POST"])
-def create_dojo():
+# # ---------- Create New Dojo Post ----------- done
+# @app.route('/create_dojo', methods=["POST"])
+# def create_dojo():
     
-    data = {
-        "fname": request.form["fname"],
-        "lname" : request.form["lname"],
-        "eaddress" : request.form["eaddress"]
-    }
+#     data = {
+#         "fname": request.form["fname"],
+#         "lname" : request.form["lname"],
+#         "eaddress" : request.form["eaddress"]
+#     }
     
-    Dojo.save_new_dojo(data)
+#     Dojo.save_new_dojo(data)
     
-    return redirect('/')
+#     return redirect('/')
 
 
-# ---------- Show Dojo Page ----------- done
-@app.route("/dojos/<id>")
-def show(id):
+# # ---------- Show Dojo Page ----------- done
+# @app.route("/dojos/<id>")
+# def show(id):
 
-    dojos = Dojo.get_one(id)
-    print(dojos)
-    return render_template("read_one.html", all_dojos = dojos, dojo_id = id)
+#     dojos = Dojo.get_one(id)
+#     print(dojos)
+#     return render_template("read_one.html", all_dojos = dojos, dojo_id = id)
 
 
-# ---------- Edit Dojo Page -----------
-@app.route("/dojos/<id>/edit")
-def edit(id):
+# # ---------- Edit Dojo Page -----------
+# @app.route("/dojos/<id>/edit")
+# def edit(id):
     
-    dojos = Dojo.get_one(id)
-    print(dojos)
-    return render_template("edit.html", all_dojos = dojos, dojo_id = id)
+#     dojos = Dojo.get_one(id)
+#     print(dojos)
+#     return render_template("edit.html", all_dojos = dojos, dojo_id = id)
 
 
-# ---------- Edit Dojo Post -----------
-@app.route('/edit_dojo/<id>', methods=["POST"])
-def edit_dojo(id):
+# # ---------- Edit Dojo Post -----------
+# @app.route('/edit_dojo/<id>', methods=["POST"])
+# def edit_dojo(id):
 
-    data = {
-        "id": id,
-        "fname": request.form["fname"],
-        "lname" : request.form["lname"],
-        "eaddress" : request.form["eaddress"]
-    }
+#     data = {
+#         "id": id,
+#         "fname": request.form["fname"],
+#         "lname" : request.form["lname"],
+#         "eaddress" : request.form["eaddress"]
+#     }
 
-    Dojo.edit_dojo(data)
-    return redirect('/')
+#     Dojo.edit_dojo(data)
+#     return redirect('/')
 
 
-# ---------- Delete Dojo Post ----------- done
-@app.route('/dojos/<int:id>/destroy')
-def delete_dojo(id):
+# # ---------- Delete Dojo Post ----------- done
+# @app.route('/dojos/<int:id>/destroy')
+# def delete_dojo(id):
     
-    data = {"id": id}
-    Dojo.delete(data)
-    return redirect('/')
+#     data = {"id": id}
+#     Dojo.delete(data)
+#     return redirect('/')
--- 2_Python/flask_mysql/dojos_and_ninjas/flask_app/models/dojo.py
+++ 2_Python/flask_mysql/dojos_and_ninjas/flask_app/models/dojo.py
@@ -3,9 +3,7 @@ from flask_app.config.mysqlconnection import connectToMySQL
 class Dojo:
     def __init__(self, data):
         self.id = data['id']
-        self.first_name = data['first_name']
-        self.last_name = data['last_name']
-        self.email = data['email']
+        self.name = data['name']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
@@ -21,33 +19,33 @@ class Dojo:
         return dojos
 
 
-    # ---------- Get One -----------
-    @classmethod
-    def get_one(cls,id):
-        query = "SELECT * FROM dojos WHERE id = " + id + ";"
-        results = connectToMySQL('dojos_and_ninjas').query_db(query)
-        dojos = []
-        for dojo in results:
-            dojos.append(cls(dojo))
-        return dojos
+    # # ---------- Get One -----------
+    # @classmethod
+    # def get_one(cls,id):
+    #     query = "SELECT * FROM dojos WHERE id = " + id + ";"
+    #     results = connectToMySQL('dojos_and_ninjas').query_db(query)
+    #     dojos = []
+    #     for dojo in results:
+    #         dojos.append(cls(dojo))
+    #     return dojos
 
 
-    # ---------- Save New Dojo -----------
-    @classmethod
-    def save_new_dojo(cls, data ):
-        query = "INSERT INTO dojos ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(eaddress)s , NOW() , NOW() );"
-        return connectToMySQL('dojos_and_ninjas').query_db( query, data )
+    # # ---------- Save New Dojo -----------
+    # @classmethod
+    # def save_new_dojo(cls, data ):
+    #     query = "INSERT INTO dojos ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(eaddress)s , NOW() , NOW() );"
+    #     return connectToMySQL('dojos_and_ninjas').query_db( query, data )
 
 
-    # ---------- Edit Dojo? -----------
-    @classmethod
-    def edit_dojo(cls, data ):
-        query = "UPDATE dojos SET first_name = %(fname)s , last_name = %(lname)s , email = %(eaddress)s , updated_at = NOW() WHERE id = %(id)s;"
-        return connectToMySQL('dojos_and_ninjas').query_db( query, data )
+    # # ---------- Edit Dojo? -----------
+    # @classmethod
+    # def edit_dojo(cls, data ):
+    #     query = "UPDATE dojos SET first_name = %(fname)s , last_name = %(lname)s , email = %(eaddress)s , updated_at = NOW() WHERE id = %(id)s;"
+    #     return connectToMySQL('dojos_and_ninjas').query_db( query, data )
 
 
-    # ---------- Delete Dojo -----------
-    @classmethod
-    def delete(cls, data ):
-        query = "DELETE FROM dojos WHERE id = %(id)s;"
-        return connectToMySQL('dojos_and_ninjas').query_db( query, data )
+    # # ---------- Delete Dojo -----------
+    # @classmethod
+    # def delete(cls, data ):
+    #     query = "DELETE FROM dojos WHERE id = %(id)s;"
+    #     return connectToMySQL('dojos_and_ninjas').query_db( query, data )
