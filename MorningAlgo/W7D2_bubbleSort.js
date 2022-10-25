a3c4db0344c187c3960b5a2743e152a4ccbf15e9
Joshua Valdez <josh@augnos.com>
2022-10-25 10:53:23 -0700

    bubblesort updates

--- MorningAlgo/W7D2_bubbleSort.js
+++ MorningAlgo/W7D2_bubbleSort.js
@@ -6,9 +6,13 @@ nums1 = [6, 3, 7, 4, 5];
 function bubbleSort (arr) {
     for (i=0; i < arr.length; i++){
         for (j=0; j < arr.length-1-i; j++){
-            if(arr[j] > arr[j+1]) [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+            if(arr[j] > arr[j+1]) {
+                console.log(arr);
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+            }
         }
     }
+    console.log("sorted!");
     return arr;
 }
 
