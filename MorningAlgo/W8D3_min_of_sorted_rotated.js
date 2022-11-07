95fed3baa0a4711714d5d2f7c33e37278bf7f5bd
Joshua Valdez <josh@augnos.com>
2022-11-06 23:30:45 -0800

    started jokes api

--- MorningAlgo/W8D3_min_of_sorted_rotated.js
+++ MorningAlgo/W8D3_min_of_sorted_rotated.js
@@ -12,7 +12,7 @@ const arr1 = [5, 6, 7, 8, 9, 10, 11, 12, 13, 1]
 // return 3
 
 
-function minSortedRotated(arr, low = 0, mid = Math.floor(arr.length / 2), high = arr.length - 1) {
+function findMinSlow(arr, low = 0, mid = Math.floor(arr.length / 2), high = arr.length - 1) {
     // printing the array and each variable at every recursion
     console.log(arr, low, mid, high);
 
@@ -22,8 +22,23 @@ function minSortedRotated(arr, low = 0, mid = Math.floor(arr.length / 2), high =
     if (arr[mid] < arr[mid - 1]) return arr[mid];
 
     // slice the array in half, and recurse only the half where slice[low] > slice[high]
-    if (arr[low] > arr[mid]) return minSortedRotated(arr.slice(0, mid));
-    if (arr[mid] > arr[high]) return minSortedRotated(arr.slice(mid, high + 1));
+    if (arr[low] > arr[mid]) return findMinSlow(arr.slice(0, mid));
+    if (arr[mid] > arr[high]) return findMinSlow(arr.slice(mid, high + 1));
 }
 
-console.log(minSortedRotated(arr1));
+console.log(findMinSlow(arr1));
+
+
+function findMin(arr, low = 0, mid = Math.floor(arr.length / 2), high = arr.length - 1) {
+    // printing the array and each variable at every recursion
+    console.log(arr, low, mid, high);
+
+    // base cases
+    if (arr[low] == arr[high]) return arr[low];
+    if (arr[low] < arr[high]) return arr[low];
+    if (arr[mid] < arr[mid - 1]) return arr[mid];
+
+    // slice the array in half, and recurse only the half where slice[low] > slice[high]
+    if (arr[low] > arr[mid]) return findMinSlow(arr.slice(0, mid));
+    if (arr[mid] > arr[high]) return findMinSlow(arr.slice(mid, high + 1));
+}
