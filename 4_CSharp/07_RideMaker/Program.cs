e83418515d67b959cc1e0fcbedad5bccbb12310b
Joshua Valdez <josh@augnos.com>
2022-11-29 12:45:37 -0800

    Completed RideMaker

---
+++ 4_CSharp/07_RideMaker/Program.cs
@@ -0,0 +1,10 @@
+﻿Vehicle FordFocus = new Vehicle("Ford Focus", 5, "Black", true, 95);
+Vehicle ToyotaPrius = new Vehicle("Toyota Prius", 5, "Yellow", true, 90);
+Vehicle Skateboard = new Vehicle("skateboard", "green");
+Vehicle Airplane = new Vehicle("Airplane", 170, "White", true, 700);
+
+List<Vehicle> AllVehicles = new List<Vehicle> {FordFocus, ToyotaPrius, Skateboard, Airplane};
+
+foreach(Vehicle x in AllVehicles) x.ShowInfo();
+FordFocus.Travel(100);
+FordFocus.ShowInfo();
