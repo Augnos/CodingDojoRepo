56659cf283329f0912970627d091e4740824082f
Joshua Valdez <josh@augnos.com>
2022-12-08 15:14:07 -0800

    Started CRUDelicious

---
+++ 4_CSharp/27_CRUDelicious/obj/Dish.cs
@@ -0,0 +1,17 @@
+#pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
+namespace _27_CRUDelicious.Models;
+
+public class Dish
+{
+    [Key]
+    public int DishId {get;set;}
+    public string Name {get;set;}
+    public string Chef {get;set;}
+    public int Tastiness {get;set;}
+    public int Calories {get;set;}
+    public string Description {get;set;}
+    public DateTime CreatedAt {get;set;} = DateTime.Now;
+    public DateTime UpdatedAt {get;set;} = DateTime.Now;
+
+}
